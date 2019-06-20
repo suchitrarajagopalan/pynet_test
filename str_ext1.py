584fff7a4769c8502352b4e6812837cc787c36d3
suchitra <suchitra.rajagopalan@cdk.com>
2019-06-20 14:02:54 -0700

    added first py exercise file

---
+++ str_ext1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from __future__ import print_function
+name1 = "suchi"
+name2 = "surya"
+name3 = "veer"
+name4 = input("enter fourth name")
+print("Hello,World")
+print("{:>30}".format(name1))
+print("{:>30}".format(name2))
+print("{:>30}".format(name3))
+print("{:>30}".format(name4))
